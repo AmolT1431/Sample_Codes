05e9ce05d33ad03dfba8698db94fd88a38c62c97
amol <amolt.com>
2024-02-12 20:36:50 +0530

    This is sample of C++

---
+++ C++/Build/C++/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: F:/C & C++/C++
+# Build directory: F:/C & C++/C++/Build/C++
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
